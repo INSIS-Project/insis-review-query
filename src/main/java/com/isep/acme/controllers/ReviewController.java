9684f2680914ca74b8365093a31d2e07c69fd034
marcellodinizr <marcellodinizrocha@gmail.com>
2023-04-08 16:43:16 +0100

    CQRS: Implement pattern

--- src/main/java/com/isep/acme/controllers/ReviewController.java
+++ src/main/java/com/isep/acme/controllers/ReviewController.java
@@ -4,11 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import com.isep.acme.dtos.ReviewDTO;
-import com.isep.acme.dtos.usecases.CreateReviewDTO;
 import com.isep.acme.services.ReviewService;
 
 import java.util.List;
@@ -39,35 +37,6 @@ class ReviewController {
 		return ResponseEntity.ok().body(review);
 	}
 
-	@Operation(summary = "creates review")
-	@PostMapping("/products/{sku}/reviews")
-	public ResponseEntity<ReviewDTO> createReview(@PathVariable(value = "sku") final String sku,
-			@RequestBody CreateReviewDTO createReviewDTO) {
-
-		final var review = rService.create(createReviewDTO, sku);
-
-		if (review == null) {
-			return ResponseEntity.badRequest().build();
-		}
-
-		return new ResponseEntity<ReviewDTO>(review, HttpStatus.CREATED);
-	}
-
-	@Operation(summary = "deletes review")
-	@DeleteMapping("/reviews/{reviewID}")
-	public ResponseEntity<Boolean> deleteReview(@PathVariable(value = "reviewID") final Long reviewID) {
-
-		Boolean rev = rService.DeleteReview(reviewID);
-
-		if (rev == null)
-			return ResponseEntity.notFound().build();
-
-		if (rev == false)
-			return ResponseEntity.unprocessableEntity().build();
-
-		return ResponseEntity.ok().body(rev);
-	}
-
 	@Operation(summary = "gets pedding reviews")
 	@GetMapping("/reviews/pending")
 	public ResponseEntity<List<ReviewDTO>> getPendingReview() {
@@ -77,19 +46,4 @@ class ReviewController {
 		return ResponseEntity.ok().body(r);
 	}
 
-	@Operation(summary = "Accept or reject review")
-	@PutMapping("/reviews/acceptreject/{reviewID}")
-	public ResponseEntity<ReviewDTO> putAcceptRejectReview(@PathVariable(value = "reviewID") final Long reviewID,
-			@RequestBody String approved) {
-
-		try {
-			ReviewDTO rev = rService.moderateReview(reviewID, approved);
-
-			return ResponseEntity.ok().body(rev);
-		} catch (IllegalArgumentException e) {
-			return ResponseEntity.badRequest().build();
-		} catch (ResourceNotFoundException e) {
-			return ResponseEntity.notFound().build();
-		}
-	}
 }
--- src/main/java/com/isep/acme/repositories/ReviewRepository.java
+++ src/main/java/com/isep/acme/repositories/ReviewRepository.java
@@ -12,16 +12,11 @@ import java.util.Optional;
 
 public interface ReviewRepository extends CrudRepository<Review, Long> {
 
-	// Optional<Review> findById(Long productId);
-
 	@Query("SELECT r FROM Review r WHERE r.product=:product ORDER BY r.publishingDate DESC")
 	Optional<List<Review>> findByProductId(Product product);
 
 	@Query("SELECT r FROM Review r WHERE r.approvalStatus='pending'")
 	Optional<List<Review>> findPendingReviews();
-
-	@Query("SELECT r FROM Review r WHERE r.approvalStatus='active'")
-	Optional<List<Review>> findActiveReviews();
 
 	@Query("SELECT r FROM Review r WHERE r.product=:product AND r.approvalStatus=:status ORDER BY r.publishingDate DESC")
 	Optional<List<Review>> findByProductIdStatus(Product product, String status);
